547ded671c87fb3cb49fadd99da0cd9cfa58d92d
sarikadara <sarika.dara96@gmail.com>
2020-06-23 20:52:16 +0530

    express

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+const app = express();
+//init middleware
+//app.useaaaaa(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// members api routes
+app.use('/api/members',require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
